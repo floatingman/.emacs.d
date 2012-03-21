fe12cb53c5fd12ecffce2d1a1c1cf2f80129c65c
floatingman <dwnewman78@gmail.com>
2012-03-21 12:28:24 -0500

    added a bunch to .gitignore

--- org-clock-save.el
+++
@@ -1,3 +0,0 @@
-;; org-persist.el - WGS-DNEWMAN-D01 at <2012-03-15 Thu 17:14>
-(setq resume-clock '("d:/home/git/org/gfs.org" . 8911))
-(setq stored-clock-history '(("d:/home/git/org/gfs.org" . 8192) ("d:/home/git/org/usbank/usbank.org" . 54391) ("d:/home/git/org/todo.org" . 3857) ("d:/home/git/org/gfs.org" . 8857)))
