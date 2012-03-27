dfcdd181fc2d322d9f9acd26ba0a4067ee8cc5ae
floatingman <dwnewman78@gmail.com>
2012-03-27 11:45:12 -0500

    delete auto-complete from submodule and twilight color theme

--- startup.d/41auto-complete.el
+++ startup.d/41auto-complete.el
@@ -7,6 +7,10 @@
 ;; Do What I Mean mode
 (setq ac-dwim t)
 
+(setq ac-modes (quote (emacs-lisp-mode lisp-interaction-mode c-mode cc-mode c++-mode clojure-mode java-mode perl-mode cperl-mode python-mode ruby-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode haml-mode sass-mode)))
+
+(setq ac-quick-help-delay 1.0)
+
 ;; custom keybindings to use tab, enter and up and down arrows
 (define-key ac-complete-mode-map "\t" 'ac-expand)
 (define-key ac-complete-mode-map "\r" 'ac-complete)
