08c11dbe010ad595406e9036a9db1230a1668aae
floatingman <dwnewman78@gmail.com>
2012-04-23 15:24:10 -0500

    added some more functions

---
+++ startup.d/05header2.el
@@ -0,0 +1,9 @@
+
+;;auto update header on file save
+
+;(add-hook 'write-file-hooks 'auto-update-file-header)
+
+
+;;insert header information on new file creation
+;(add-hook 'emacs-lisp-mode-hook 'auto-make-header)
+;(add-hook 'c-mode-common-hook   'auto-make-header)
