005990d431619acd6a99c4f065df67c98ab21e04
floatingman <dwnewman78@gmail.com>
2012-07-23 10:27:57 -0500

    readded yasnippets mode

--- startup.d/12sacha.el
+++ startup.d/12sacha.el
@@ -61,3 +61,42 @@
 ;; better undo handling
 (require 'undo-tree)
 (global-undo-tree-mode)
+
+;;count frequently used keys
+(require 'keyfreq)
+(keyfreq-mode 1)
+(keyfreq-autosave-mode 1)
+
+;;avoid weasel words
+(require 'artbollocks-mode)
+;; Avoid these phrases
+(setq weasel-words-regex
+      (concat "\\b" (regexp-opt
+                     '("one of the"
+                       "should"
+                       "just"
+                       "sort of"
+                       "a lot"
+                       "probably"
+                       "maybe"
+                       "perhaps"
+                       "I think"
+                       "really"
+                       "pretty"
+                       "maybe"
+                       "nice"
+                       "action"
+                       "utilize"
+                       "leverage") t) "\\b"))
+;; Fix a bug in the regular expression to catch repeated words
+(setq lexical-illusions-regex "\\b\\(\\w+\\)\\W+\\(\\1\\)\\b")
+;; Don't show the art critic words, or at least until I figure
+;; out my own jargon
+(setq artbollocks nil)
+;; Make sure keywords are case-insensitive
+(defadvice search-for-keyword (around sacha activate)
+  "Match in a case-insensitive way."
+  (let ((case-fold-search t))
+    ad-do-it))
+
+(add-hook 'org-capture-mode-hook 'artbollocks-mode)
