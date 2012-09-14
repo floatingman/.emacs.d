c57bfcd6d7cb06fad08cb958171d0a79d9a75e34
floatingman <dwnewman78@gmail.com>
2012-09-14 16:31:24 -0500

    changed some key bindings around

--- startup.d/99bindings.el
+++ startup.d/99bindings.el
@@ -110,7 +110,7 @@
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "\C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+;(global-set-key (kbd "C-M-r") 'isearch-backward)
 ;; File Finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
 (global-set-key (kbd "C-x C-p") 'find-file-at-point)
