fa2444501c4e842b6a687a6e031a38ab4e3d78cd
floatingman <dwnewman78@gmail.com>
2013-08-13 10:26:22 -0500

    modified style
    modified style of init-spelling.el

--- init-spelling.el
+++ init-spelling.el
@@ -1,6 +1,12 @@
+;;; init-spelling --- Summary
+;;; Commentary:
+
+;;; Code:
+
 (require 'ispell)
 
 (when (executable-find ispell-program-name)
   (require 'init-flyspell))
 
 (provide 'init-spelling)
+;;; init-spelling.el ends here
