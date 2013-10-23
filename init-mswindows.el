7065c31698559df3af65fde9328ecf0226331e58
floatingman <dwnewman78@gmail.com>
2013-10-23 15:47:00 -0500

    added function to display tabs and hide messages with cygwin

--- init-mswindows.el
+++ init-mswindows.el
@@ -1,5 +1,6 @@
 (when *is-windows*
   (setenv "PATH" (concat "C:\\cygwin\bin;" (getenv "PATH")))
+  (setq null-device "/dev/null")
   (setq find-program "C:\\cygwin\\bin\\find.exe"
         grep-program "C:\\cygwin\\bin\\grep.exe"))
 
