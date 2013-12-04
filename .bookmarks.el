c2a92f7e9f4fbb151939e90e608a50665898f917
floatingman <dwnewman78@gmail.com>
2013-12-04 22:23:28 +0000

    updated org-mode

---
+++ .bookmarks.el
@@ -0,0 +1,15 @@
+;;;; Emacs Bookmark Format Version 1 ;;;;
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("org-refile-last-stored"
+ (filename . "~/git/org/gfs.org")
+ (front-context-string . "** TODO Automati")
+ (rear-context-string . "0-17 Thu 09:14]\n")
+ (position . 108868))
+("org-capture-last-stored"
+ (filename . "~/git/org/refile.org")
+ (front-context-string . "* TODO Automatio")
+ (rear-context-string . "2-02 Mon 14:55]\n")
+ (position . 75))
+)
