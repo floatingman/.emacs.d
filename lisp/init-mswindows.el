5bb0306e2776cd32e6d1ba0b4e9943ae3b07dd1d
Daniel Newman <dwnewman78@gmail.com>
2015-01-13 19:02:18 -0600

    starting a new config

--- lisp/init-mswindows.el
+++
@@ -1,7 +0,0 @@
-(when *is-windows*
-  (setenv "PATH" (concat "C:\\utils\\cygwin\bin;" (getenv "PATH")))
-  (setq null-device "/dev/null")
-  (setq find-program "C:\\utils\\cygwin\\bin\\find.exe"
-        grep-program "C:\\utils\\cygwin\\bin\\grep.exe"))
-
-(provide 'init-mswindows)
