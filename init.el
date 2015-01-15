36b6848728da2ed7d90d9571d16035db6cfbea18
Daniel Newman <dwnewman78@gmail.com>
2015-01-15 11:09:12 -0600

    added options to save history

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; This sets up the load path so that we can override it
 (package-initialize nil)
 
+;; Override the packages with the the git version of Org and other packages
+(add-to-list 'load-path "~/elisp/org-mode/lisp")
+(add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
 ;; Load the rest of the packages
 (package-initialize t)
