e143ad01d9403f5038c9ed18899611b371822ca9
Daniel Newman <dwnewman78@gmail.com>
2015-02-09 09:42:22 -0600

    reverted back to old org-mode config
    I couldn't get the literate config to work so I'm goint go have to
    see about converting my old config to a literate form

--- init.el
+++ init.el
@@ -5,8 +5,36 @@
 (add-to-list 'load-path "~/elisp/org-mode/lisp")
 (add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Initialize a few variables
+(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-windows* (eq system-type 'windows-nt))
+(defconst *is-linux* (eq system-type 'gnu/linux))
+(defconst *is-gui* (not (eq window-system nil)))
+
+;; From old config
+(require 'init-benchmarking) ;; Measure startup time
+(require 'init-compat)
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+(require 'init-org) ;; load org-mode
+
 ;; Load the rest of the packages
 (package-initialize t)
 (setq package-enable-at-startup nil)
 (org-babel-load-file "~/.emacs.d/dnewman.org")
-(org-babel-load-file "~/.emacs.d/org-mode.org")
+
+
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (message "init completed in %.2fms"
+                     (sanityinc/time-subtract-millis after-init-time before-init-time))))
+
+
+(provide 'init)
