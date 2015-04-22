de1ccfa45c12208b9f669b8429cf77d6a6b40765
Daniel Newman <dwnewman78@gmail.com>
2015-04-22 13:59:07 -0500

    more reconciling of config

--- init.el
+++ init.el
@@ -1,23 +1,14 @@
-;; This sets up the load path so that we can override it
-(package-initialize nil)
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+
+(let ((minver "23.3"))
+  (when (version<= emacs-version "23.1")
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 ;; Override the packages with the the git version of Org and other packages
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-;; Initialize a few variables
-(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
-(defconst *is-a-mac* (eq system-type 'darwin))
-(defconst *is-windows* (eq system-type 'windows-nt))
-(defconst *is-linux* (eq system-type 'gnu/linux))
-(defconst *is-gui* (not (eq window-system nil)))
-
-;; From old config
-;(require 'init-benchmarking) ;; Measure startup time
-;(require 'init-compat)
-;(require 'init-utils)
-;(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
-;(require 'init-elpa)      ;; Machinery for installing required packages
-;(require 'init-exec-path) ;; Set up $PATH
 
 ;; Load the rest of the packages
 (package-initialize t)
@@ -27,13 +18,6 @@
 (require 'init-org) ;; load org-mode
 (require 'init-mswindows)
 
-(require-package 'smart-mode-line)
-(require-package 'guide-key)
-(require-package 'key-chord)
-(require-package 'smartscan)
-(require-package 'artbollocks-mode)
-(require-package 'tern)
-(require-package '2048-game)
 
 
 
