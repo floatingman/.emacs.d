edfe4015749dadb714380a7def430c3e187a0202
Daniel Newman <dwnewman78@gmail.com>
2015-04-23 14:07:21 -0500

    added highlighting of vc files in dired

--- init.el
+++ init.el
@@ -30,3 +30,24 @@
 
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((no-byte-compile t)
+     (ruby-compilation-executable . "ruby")
+     (ruby-compilation-executable . "ruby1.8")
+     (ruby-compilation-executable . "ruby1.9")
+     (ruby-compilation-executable . "rbx")
+     (ruby-compilation-executable . "jruby")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-input-face ((t (:foreground "antique white"))))
+ '(helm-selection ((t (:background "ForestGreen" :foreground "black"))))
+ '(outline-1 ((t (:inherit font-lock-function-name-face :foreground "cornflower blue")))))
