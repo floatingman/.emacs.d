04844e2ef16134ee301b8cb8f7d8492a02b1cf13
Daniel Newman <dwnewman78@gmail.com>
2016-02-10 17:59:30 +0000

    monolithic emacs configs are so ughh

--- init.el
+++ init.el
@@ -1,28 +1,18 @@
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
-(let ((minver "23.3"))
-  (when (version<= emacs-version "23.1")
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
-
-;; This sets up the load path so that we can override it
-(package-initialize nil)
-
 ;; Override the packages with the the git version of Org and other packages
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org-mode/lisp"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org-mode/contrib/lisp"))
-;;(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/lisp")
-;;(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/contrib/lisp")
+
 
 
 
 ;; Load the rest of the packages
 (package-initialize nil)
 (setq package-enable-at-startup nil)
-(org-babel-load-file "~/.emacs.d/dnewman.org")
+;;(org-babel-load-file "~/.emacs.d/dnewman.org")
 
 ;; Load some custom configs 
 (require 'init-org) ;; load org-mode
