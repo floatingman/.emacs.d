194bd97964df82d8d495e01eb320f6a497ee5444
Daniel Newman <dwnewman78@gmail.com>
2016-02-22 19:41:11 -0600

    Changed erc to only logon after being called

--- lisp/init-chat.el
+++ lisp/init-chat.el
@@ -31,9 +31,21 @@
     "Deop myself from current channel."
     (erc-cmd-DEOP (format "%s" (erc-current-nick)))))
 
+(defmacro de-erc-connect (command server port nick)
+	"Create interactive command `command', for connecting to an IRC server. The
+      command uses interactive mode if passed an argument."
+	(fset command
+				`(lambda (arg)
+					 (interactive "p")
+					 (if (not (= 1 arg))
+							 (call-interactively 'erc)
+						 (erc :server ,server :port ,port :nick ,nick)))))
 
+
+(de-erc-connect erc-freenode "irc.freenode.net" 6667 "floatingman")
+(de-erc-connect erc-bitlbee "thenewmans.no-ip.org" 6667 "floatingman")
 ;;(erc :server "irc.freenode.net" :port 6667 :nick "floatingman")
-(erc :server "thenewmans.no-ip.org" :port 6667 :nick "floatingman")
+;;(erc :server "thenewmans.no-ip.org" :port 6667 :nick "floatingman")
 
 (defun bitlbee-identify () 
   "If we're on the bitlbee server, send the identify command to the #bitlbee channel."
