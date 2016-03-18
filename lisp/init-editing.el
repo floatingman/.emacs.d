488ccd2c7d96d06090eecb6d56cfd99162b2cb4b
Daniel Newman <dwnewman78@gmail.com>
2016-03-18 16:05:51 -0500

    added whitespace mode

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -41,5 +41,38 @@
 (use-package fill-column-indicator
 	:ensure t)
 
+;; Whitespace mode from the awesome site http://writequit.org/org/settings.html
+(setq whitespace-line-column 140)
+
+(setq whitespace-style '(tabs newline space-mark
+                              tab-mark newline-mark
+                              face lines-tail))
+(setq whitespace-display-mappings
+      ;; all numbers are Unicode codepoint in decimal. e.g. (insert-char 182 1)
+      ;; 32 SPACE, 183 MIDDLE DOT
+      '((space-mark nil)
+        ;; 10 LINE FEED
+        ;; (newline-mark 10 [172 10])
+        (newline-mark nil)
+        ;; 9 TAB, MIDDLE DOT
+        (tab-mark 9 [183 9] [92 9])))
+
+(setq whitespace-global-modes '(not org-mode
+                                    eshell-mode
+                                    shell-mode
+                                    web-mode
+                                    log4j-mode
+                                    "Web"
+                                    dired-mode
+                                    emacs-lisp-mode
+                                    clojure-mode
+                                    lisp-mode))
+
+;; turn on whitespace mode globally
+(global-whitespace-mode 1)
+(diminish 'global-whitespace-mode "")
+(set-default 'indicate-empty-lines t)
+(setq show-trailing-whitespace t)
+
 (provide 'init-editing)
 ;;; init-editing.el ends here
