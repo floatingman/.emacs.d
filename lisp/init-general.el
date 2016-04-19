ffc8e4956ab6d1dd0cd282f0500f45c844308e62
Daniel Newman <dwnewman78@gmail.com>
2016-04-19 09:59:32 -0500

    restored mouse scrolling added mouse copy function

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -191,14 +191,18 @@
   (menu-bar-mode -1))
 (when (functionp 'set-scroll-bar-mode)
   (set-scroll-bar-mode 'nil))
-(when (functionp 'mouse-wheel-mode)
-  (mouse-wheel-mode -1))
 (when (functionp 'tooltip-mode)
   (tooltip-mode -1))
 (when (functionp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (functionp 'blink-cursor-mode)
   (blink-cursor-mode -1))
+
+(use-package mouse-copy
+  :bind
+  ("C-down-mouse-1" . mouse-drag-secondary-pasting)
+  ("C-S-down-mouse-1" . mouse-drag-secondary-moving)
+  )
 
 ;; no beeping no startup messages
 (setq ring-bell-function (lambda ()))
@@ -231,7 +235,6 @@
 (setq require-final-newline t)
 
 ;; use regex for searching cause it's fun
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 
