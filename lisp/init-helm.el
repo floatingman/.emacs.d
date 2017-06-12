da779ed3604eba64e01a679157fc91853e642a4d
Daniel Newman <dwnewman78@gmail.com>
2017-06-12 11:16:49 -0500

    Switching helm-descbinds to subtree

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,47 +1,3 @@
-(use-package helm-descbinds
-  :ensure t
-  :bind ("C-h b" . helm-descbinds)
-  :init (fset 'describe-bindings 'helm-descbinds)
-  :config (require 'helm-config))
-
-(use-package helm-ag
-  :ensure t)
-
-(use-package helm-flx
-  :ensure t)
-
-(use-package helm-flycheck
-  :ensure t)
-
-(use-package helm-flyspell
-  :ensure t)
-
-(use-package helm-gtags
-  :ensure t)
-
-(use-package helm-css-scss
-  :ensure t)
-
-(use-package helm-swoop
-  :load-path "site-lisp/helm-swoop"
-  :bind
-  (("M-s o" . helm-swoop)
-   ("M-I" . helm-swoop-back-to-last-)
-   ("M-s /" . helm-multi-swoop))
-  :config
-  ;; When doing isearch, hand the word over to helm-swoop
-  (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-  ;; From helm-swoop to helm-multi-swoop-all
-  (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-  ;; Save buffer when helm-multi-swoop-edit complete
-  (setq helm-multi-swoop-edit-save t
-        ;; If this value is t, split window inside the current window
-        helm-swoop-split-with-multiple-windows t
-        ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
-        helm-swoop-split-direction 'split-window-vertically
-        ;; If nil, you can slightly boost invoke speed in exchange for text color
-        helm-swoop-speed-or-color nil))
-
 (use-package helm-config
   :if (not running-alternate-emacs)
   :demand t
@@ -71,6 +27,52 @@
 
   (when (executable-find "curl")
     (setq helm-google-suggest-use-curl-p t)))
+
+(use-package helm-swoop
+  :load-path "site-lisp/helm-swoop"
+  :bind
+  (("M-s o" . helm-swoop)
+   ("M-I" . helm-swoop-back-to-last-)
+   ("M-s /" . helm-multi-swoop))
+  :config
+  ;; When doing isearch, hand the word over to helm-swoop
+  (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+  ;; From helm-swoop to helm-multi-swoop-all
+  (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+  ;; Save buffer when helm-multi-swoop-edit complete
+  (setq helm-multi-swoop-edit-save t
+        ;; If this value is t, split window inside the current window
+        helm-swoop-split-with-multiple-windows t
+        ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+        helm-swoop-split-direction 'split-window-vertically
+        ;; If nil, you can slightly boost invoke speed in exchange for text color
+        helm-swoop-speed-or-color nil))
+
+(use-package helm-descbinds
+  :load-path "site-lisp/helm-descbinds"
+  :bind ("C-h b" . helm-descbinds)
+  :init
+  (fset 'describe-bindings 'helm-descbinds)
+  :config
+  (require 'helm-config))
+
+(use-package helm-ag
+  :ensure t)
+
+(use-package helm-flx
+  :ensure t)
+
+(use-package helm-flycheck
+  :ensure t)
+
+(use-package helm-flyspell
+  :ensure t)
+
+(use-package helm-gtags
+  :ensure t)
+
+(use-package helm-css-scss
+  :ensure t)
 
 (use-package helm-c-yasnippet
   :ensure t
