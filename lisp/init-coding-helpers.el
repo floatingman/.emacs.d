1e499e1d8d3f1e4f1ca01e0548dcbbaf6c00710d
Daniel Newman <dwnewman78@gmail.com>
2017-06-12 22:16:15 -0500

    Removed smartparens

--- lisp/init-coding-helpers.el
+++ lisp/init-coding-helpers.el
@@ -94,97 +94,13 @@
   :ensure t
   :init (rainbow-delimiters-mode))
 
-(use-package smartparens
-  :defer 5
-  :diminish smartparens-mode
-  :ensure smartparens
-  :bind (("M-9" . sp-backward-sexp)
-         ("M-0" . sp-forward-sexp))
-  :init
-  (show-smartparens-global-mode t)
-  (add-hook 'prog-mode-hook #'turn-on-smartparens-mode)
-  :config
-  (progn
-    (require 'smartparens-config)
-    (require 'smartparens-html)
-    (require 'smartparens-python)
-    (require 'smartparens-latex)
-
-    ;; Remove the M-<backspace> binding that smartparens adds
-    (let ((disabled '("M-<backspace>")))
-      (setq sp-smartparens-bindings
-            (cl-remove-if (lambda (key-command)
-                            (member (car key-command) disabled))
-                          sp-smartparens-bindings)))
-
-    (define-key sp-keymap (kbd "C-(") 'sp-forward-barf-sexp)
-    (define-key sp-keymap (kbd "C-)") 'sp-forward-slurp-sexp)
-    (define-key sp-keymap (kbd "M-(") 'sp-forward-barf-sexp)
-    (define-key sp-keymap (kbd "M-)") 'sp-forward-slurp-sexp)
-    (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
-    (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
-    (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
-    (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
-    (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
-    (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
-    (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
-    (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
-    (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
-    (define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)
-    (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
-    (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
-    ;; (define-key sp-keymap (kbd "C-M-n") 'sp-next-sexp)
-    ;; (define-key sp-keymap (kbd "C-M-p") 'sp-previous-sexp)
-    (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
-    (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
-    (define-key sp-keymap (kbd "M-D") 'sp-splice-sexp)
-    (define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
-    (define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
-    (define-key sp-keymap (kbd "C-M-]") 'sp-select-next-thing)
-    (define-key sp-keymap (kbd "M-F") 'sp-forward-symbol)
-    (define-key sp-keymap (kbd "M-B") 'sp-backward-symbol)
-    (define-key sp-keymap (kbd "H-t") 'sp-prefix-tag-object)
-    (define-key sp-keymap (kbd "H-p") 'sp-prefix-pair-object)
-    (define-key sp-keymap (kbd "H-s c") 'sp-convolute-sexp)
-    (define-key sp-keymap (kbd "H-s a") 'sp-absorb-sexp)
-    (define-key sp-keymap (kbd "H-s e") 'sp-emit-sexp)
-    (define-key sp-keymap (kbd "H-s p") 'sp-add-to-previous-sexp)
-    (define-key sp-keymap (kbd "H-s n") 'sp-add-to-next-sexp)
-    (define-key sp-keymap (kbd "H-s j") 'sp-join-sexp)
-    (define-key sp-keymap (kbd "H-s s") 'sp-split-sexp)
-
-    (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-    ;; Remove '' pairing in elisp because quoting is used a ton
-    (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
-
-    (sp-pair "%" "%" :wrap "C-%")
-    (sp-pair "<" ">" :wrap "C->") 
-
-    (sp-with-modes sp--lisp-modes
-      (sp-local-pair "(" nil :bind "C-(")))
-  )
-
 ;; flycheck for all your fly inspection needs
 (use-package flycheck
-  :ensure t
+  :load-path "site-lisp/flycheck"
   :defer 5
-  :bind (("M-g M-n" . flycheck-next-error)
-         ("M-g M-p" . flycheck-previous-error)
-         ("M-g M-=" . flycheck-list-errors))
-  :init (global-flycheck-mode)
-  :diminish flycheck-mode 
   :config
-	(progn
-		(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc json-jsonlint json-python-json))
-		(use-package flycheck-pos-tip
-		  :ensure t
-		  :init (flycheck-pos-tip-mode))
-		(use-package helm-flycheck
-		  :ensure t
-		  :init (define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
-		))
+  (defalias 'flycheck-show-error-at-point-soon 'flycheck-show-error-at-point))
 
-(setq vc-handled-backends '(SVN Git))
 
 (defun my/add-watchwords ()
   "Highlight FIXME, TODO, and NOCOMMIT in code"
@@ -193,7 +109,6 @@
           1 '((:foreground "#d7a3ad") (:weight bold)) t))))
 
 (add-hook 'prog-mode-hook #'hl-line-mode)
-
 
 (add-hook 'prog-mode-hook #'my/add-watchwords)
 
