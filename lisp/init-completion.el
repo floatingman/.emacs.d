e5c8ed7ce3d23d20a4086555a672fbc3decb6604
Daniel Newman <dwnewman78@gmail.com>
2017-06-20 21:23:45 -0500

    Added snippets

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -4,4 +4,22 @@
   (add-hook 'prog-mode-hook 'company-mode)
   )
 
+(bind-key "M-/" 'hippie-expand)
+(defun sanityinc/dabbrev-friend-buffer (other-buffer)
+  (< (buffer-size other-buffer) (* 1 1024 1024)))
+(setq dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer)
+
+(setq hippie-expand-try-functions-list
+      '(yas-hippie-try-expand
+        try-expand-all-abbrevs
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-dabbrev
+        try-expand-dabbrev-from-kill
+        try-expand-dabbrev-all-buffers
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
 (provide 'init-completion)
---
+++ lisp/init-snippets.el
@@ -0,0 +1,115 @@
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :init (yas-global-mode)
+  :config
+  (progn
+    (yas-global-mode)
+    (add-hook 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
+    (setq yas-key-syntaxes '("w_" "w_." "^ "))
+    (setq yas-installed-snippets-dir (concat user-emacs-directory "snippets"))
+    (setq yas-expand-only-for-last-commands nil)
+    (yas-global-mode 1)
+    (bind-key "\t" 'hippie-expand yas-minor-mode-map)
+    (add-to-list 'yas-prompt-functions 'shk-yas/helm-prompt)))
+
+
+(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
+  "Use helm to select a snippet. Put this into `yas/prompt-functions.'"
+  (interactive)
+  (setq display-fn (or display-fn 'identity))
+  (if (require 'helm-config)
+      (let (tmpsource cands result rmap)
+        (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+        (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+        (setq tmpsource
+              (list
+               (cons 'name prompt)
+               (cons 'candidates cands)
+               '(action . (("Expand" . (lambda (selection) selection))))
+               ))
+        (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+        (if (null result)
+            (signal 'quit "user quit!")
+          (cdr (assoc result rmap))))
+    nil))
+
+(setq yasnippet-can-fire-cursor-color "purple")
+
+;; It will test whether it can expand, if yes, cursor color -> green.
+(defun yasnippet-can-fire-p (&optional field)
+  (interactive)
+  (setq yas--condition-cache-timestamp (current-time))
+  (let (templates-and-pos)
+    (unless (and yas-expand-only-for-last-commands
+                 (not (member last-command yas-expand-only-for-last-commands)))
+      (setq templates-and-pos (if field
+                                  (save-restriction
+                                    (narrow-to-region (yas--field-start field)
+                                                      (yas--field-end field))
+                                    (yas--templates-for-key-at-point))
+                                (yas--templates-for-key-at-point))))
+    (and templates-and-pos (first templates-and-pos))))
+
+(defun my/change-cursor-color-when-can-expand (&optional field)
+  (interactive)
+  (when (eq last-command 'self-insert-command)
+    (set-cursor-color (if (my/can-expand)
+                          yasnippet-can-fire-cursor-color
+                        default-cursor-color))))
+
+(defun my/can-expand ()
+  "Return true if right after an expandable thing."
+  (or (abbrev--before-point) (yasnippet-can-fire-p)))
+
+;; As pointed out by Dmitri, this will make sure it will update color when needed.
+(remove-hook 'post-command-hook 'my/change-cursor-color-when-can-expand)
+
+(defun my/insert-space-or-expand ()
+  "For binding to the SPC SPC keychord."
+  (interactive)
+  (condition-case nil (or (my/hippie-expand-maybe nil) (insert "  "))))
+
+(defun my/hippie-expand-maybe (arg)
+  "Try to expand text before point, using multiple methods.
+The expansion functions in `hippie-expand-try-functions-list' are
+tried in order, until a possible expansion is found.  Repeated
+application of `hippie-expand' inserts successively possible
+expansions.
+With a positive numeric argument, jumps directly to the ARG next
+function in this list.  With a negative argument or just \\[universal-argument],
+undoes the expansion."
+  (interactive "P")
+  (require 'hippie-exp)
+  (if (or (not arg)
+          (and (integerp arg) (> arg 0)))
+      (let ((first (or (= he-num -1)
+                       (not (equal this-command last-command)))))
+        (if first
+            (progn
+              (setq he-num -1)
+              (setq he-tried-table nil)))
+        (if arg
+            (if (not first) (he-reset-string))
+          (setq arg 0))
+        (let ((i (max (+ he-num arg) 0)))
+          (while (not (or (>= i (length hippie-expand-try-functions-list))
+                          (apply (nth i hippie-expand-try-functions-list)
+                                 (list (= he-num i)))))
+            (setq i (1+ i)))
+          (setq he-num i))
+        (if (>= he-num (length hippie-expand-try-functions-list))
+            (progn (setq he-num -1) nil)
+          (if (and hippie-expand-verbose
+                   (not (window-minibuffer-p)))
+              (message "Using %s"
+                       (nth he-num hippie-expand-try-functions-list)))))
+    (if (and (>= he-num 0)
+             (eq (marker-buffer he-string-beg) (current-buffer)))
+        (progn
+          (setq he-num -1)
+          (he-reset-string)
+          (if (and hippie-expand-verbose
+                   (not (window-minibuffer-p)))
+              (message "Undoing expansions"))))))
+
+(provide 'init-snippets)
