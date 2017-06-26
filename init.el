13b1dfd37af0ebc6a8ad245341f8e640ecbc75d3
Daniel Newman <dwnewman78@gmail.com>
2017-06-26 14:23:53 -0500

    Added saving session

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'init-package)
+(use-package init-benchmarking)
 (use-package init-utils)
 (use-package init-ui)
 (use-package init-theme)
@@ -41,6 +42,8 @@
 (use-package init-python)
 (use-package init-grep)
 (use-package init-ivy)
+(use-package init-session)
+
 ;; turn off debugging after emacs starts
 (setq debug-on-error nil)
 (setq debug-on-quit nil)
@@ -69,7 +72,8 @@
     ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (company-anaconda multiple-cursors diff-hl dired-sort dired+ yasnippet use-package smex redshank rainbow-delimiters projectile paredit nlinum indent-guide ido-ubiquitous guide-key golden-ratio flycheck exec-path-from-shell erefactor company-quickhelp color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized auto-compile))))
+    (company-anaconda multiple-cursors diff-hl dired-sort dired+ yasnippet use-package smex redshank rainbow-delimiters projectile paredit nlinum indent-guide ido-ubiquitous guide-key golden-ratio flycheck exec-path-from-shell erefactor company-quickhelp color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized auto-compile)))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
