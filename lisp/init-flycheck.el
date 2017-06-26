810891a28b079e867c3875d576332fc4351493b5
Daniel Newman <dwnewman78@gmail.com>
2017-06-26 14:14:24 -0500

    Moved flycheck and paredit into their one config files

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -4,10 +4,4 @@
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
-(use-package paredit-everywhere
-  :ensure t
-  :config
-  (add-hook 'prog-mode-hook 'paredit-everywhere-mode))
-
-
-(provide 'init-coding)
+(provide 'init-flycheck)
--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -61,34 +61,6 @@ couldn't figure things out (ex: syntax errors)."
           (delete-region (point-min) (point))
           (insert (mapconcat 'cdr list "\n"))))))
 
-(use-package paredit
-  :ensure t
-  :commands paredit-mode
-  :diminish paredit-mode
-  :init (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-  :config
-  (use-package paredit-ext
-    :load-path "site-lisp")
-  (bind-key "C-M-l" #'paredit-recentre-on-sexp paredit-mode-map)
-
-  (bind-key ")" #'paredit-close-round-and-newline paredit-mode-map)
-  (bind-key "M-)" #'paredit-close-round paredit-mode-map)
-
-  (bind-key "M-k" #'paredit-raise-sexp paredit-mode-map)
-  (bind-key "M-I" #'paredit-splice-sexp paredit-mode-map)
-
-  (unbind-key "M-r" paredit-mode-map)
-  (unbind-key "M-s" paredit-mode-map)
-  (unbind-key "M-?" paredit-mode-map)
-
-  (bind-key "C-. D" #'paredit-forward-down paredit-mode-map)
-  (bind-key "C-. B" #'paredit-splice-sexp-killing-backward paredit-mode-map)
-  (bind-key "C-. C" #'paredit-convolute-sexp paredit-mode-map)
-  (bind-key "C-. F" #'paredit-splice-sexp-killing-forward paredit-mode-map)
-  (bind-key "C-. a" #'paredit-add-to-next-list paredit-mode-map)
-  (bind-key "C-. A" #'paredit-add-to-previous-list paredit-mode-map)
-  (bind-key "C-. j" #'paredit-join-with-next-list paredit-mode-map)
-  (bind-key "C-. J" #'paredit-join-with-previous-list paredit-mode-map))
 
 (use-package redshank
   :defer t
